e15fa6bd2305d8ff7434358390a2a7b15dcc59e3
Boopathi Rajaa <me@boopathi.in>
2012-11-26 03:12:21 +0530

    working trie

--- wordament.cpp
+++ wordament.cpp
@@ -33,6 +33,7 @@
 #define GL ({int64 t;scanf("%ld",&t);t;})
 #define DB(x) cout<<"\n"<<#x<<" = "<<(x)<<"\n";
 #define CL(a,b) memset(a,b,sizeof(a))
+#define PB push_back
 typedef long long int int64;
 typedef unsigned long long int uint64;
 const double PI=3.14159265358979323846264338327950288419716939937510582097494459230;
@@ -46,11 +47,86 @@ inline void frint(int *a){
 int gcd(int a,int b) { if(a%b==0)return b; else return gcd(b,a%b); }
 using namespace std;
 
+//trie
+class Node {
+    private:
+        char ch;
+        bool m;
+        vector<Node*> children;
+    public:
+        Node() { ch = ' '; m=false; }
+        ~Node() {}
+        char get() { return ch; }
+        void set(char c) { ch = c; }
+        bool end() { return m; }
+        void mark() { m = true; }
+        Node* find(char c);
+        void append(Node* child) { children.PB(child); }
+        vector<Node*> getChildren() { return children; }
+};
+
+class Trie {
+    private: 
+        Node* root;
+    public:
+        Trie() { root = new Node(); }
+        ~Trie() {}
+        void add(string s);
+        bool search(string s);
+};
+
+Node* Node::find(char c) {
+    int l = children.size();
+    FOR(i,l) {
+        Node* tmp = children[i];
+        if(tmp->get() == c) return tmp;
+    }
+    return NULL;
+}
+
+void Trie::add(string s) {
+    Node* current = root;
+    int l = s.length();
+    if(l == 0) return current->mark();
+    FOR(i,l) {
+        Node* child = current->find(s[i]);
+        if(child != NULL) current=child;
+        else {
+            Node* tmp = new Node();
+            tmp->set(s[i]);
+            current->append(tmp);
+            current=tmp;
+        }
+        if(l-1 == i) current->mark();
+    }
+}
+
+bool Trie::search(string s) {
+    Node* current = root;
+    int l = s.length();
+    while(current != NULL) {
+        FOR(i,l) {
+            Node* tmp = current->find(s[i]);
+            if(tmp == NULL) return false;
+            current=tmp;
+        }
+        if(current->end()) return true;
+        else return false;
+    }
+    return false;
+}
+
 int main() {
 	int n=4;
 	char a[5][5];
-	FOR(i,n) FOR(j,n) cin>>a[i][j];
-	ifstream words("./wordlist");
+    char buffer[40];
+    int c=0;
+    Trie* db = new Trie();
+    while(scanf("%s", buffer) == 1) {
+        db->add(string(buffer));
+    }
+    FOR(i,502992)
+    	cout<<db->search("search")<<endl;
 	return 0;
 }
 
