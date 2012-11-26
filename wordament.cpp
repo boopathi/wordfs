14057c7145823693e004218473350b8921c61f9d
Boopathi Rajaa <me@boopathi.in>
2012-11-26 17:45:39 +0530

    Matrix search dfs - NOT WORKING

--- wordament.cpp
+++ wordament.cpp
@@ -99,22 +99,75 @@ bool Trie::search(string s) {
 }
 
 //Matrix - Find a string in matrix
-char matrix[5][5];
-bool matrixSearch(char s[40]) {
-	int l=strlen(s);
-	
+char M[4][4] = { 
+	'i', 'p', 'r', 'x',
+	'e', 'd', 'e', 'm',
+	'g', 't', 'a', 't',
+	'h', 'g', 'i', 't'
+} ;
+
+bool dfs(char s[40], int l, int x, int i, int j, bool (&v)[4][4]) {
+	//surrounding chars
+	//M[i-1][j-1],	M[i-1][j],	M[i-1][j+1]
+	//M[i][j-1],	M[i][j],	M[i][j+1]
+	//M[i+1][j-1],	M[i+1][j],	M[i+1][j+1]
+	//current element = s[x] where s[x-1] is found
+	//boundary
+	if(i<0 || i>4 || j<0 || j>4) return false;
+	//if visited,
+	if(v[i][j]) return false;
+	//for i,j
+	if(s[x] == M[i][j]) {
+		if(x==l) return true;
+		v[i][j]=true;
+		bool result = 	dfs(s,l,x+1,i-1,j-1,v) ||
+						dfs(s,l,x+1,i-1,j,v) ||
+						dfs(s,l,x+1,i-1,j+1,v) ||
+						dfs(s,l,x+1,i,j-1,v) ||
+						dfs(s,l,x+1,i,j+1,v) ||
+						dfs(s,l,x+1,i+1,j-1,v) ||
+						dfs(s,l,x+1,i+1,j,v) ||
+						dfs(s,l,x+1,i+1,j+1,v);
+		//backtrack
+		v[i][j]=false;
+		return result;
+	}
+	else return false;
 }
 
-int main() {
+bool matrixSearch(string s) {
+	int l = s.length();
+	bool v[4][4]; FOR(i,4) FOR(j,4) v[i][j]=false;
+	//find the starting points
+	FOR(i,4) FOR(j,4) if(s[0] == M[i][j]) 
+		if(dfs(strdup(s.c_str()),l,0,i,j,v)) return true;
+	return false;
+}
+
+int main(int argc, char* argv[]) {
 	int n=4;
-    char buffer[40];
-    int c=0;
+    char buffer[40]; 
+    int c=0;//number of words
     Trie* db = new Trie();
-    while(scanf("%s", buffer) == 1) {
-        db->add(string(buffer));
+    //populate the trie
+    string file(argc==2 ? argv[1] : "wordlist");
+    ifstream ff(file.c_str());
+    while(true) {
+    	ff>>buffer;
+    	db->add(string(buffer));
+    	if(ff.eof()) break;
     }
-    FOR(i,502992)
-    	cout<<db->search("search")<<endl;
+    cout<<"Not start searching : "<<endl;
+    /*while(scanf("%s", buffer) == 1) {
+        db->add(string(buffer));
+    }*/
+    int t=GI;
+    while(t-->0) {
+    	char str[20];
+    	scanf("%s",str);
+    	if(matrixSearch(string(str))) cout<<"Found"<<endl;
+    	else cout<<"NOT Found"<<endl;
+    }
 	return 0;
 }
 
