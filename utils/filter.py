cb2d618e6d1b9ee97464be8944147245b60df2ad
Boopathi Rajaa <me@boopathi.in>
2012-11-30 13:34:12 +0530

    Removed Unnecessary code

--- utils/filter.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-f = open("./words")
-valid = lambda x: len(x)>2 and len(x)<18 and "'" not in x
-lines = { line.lower().strip(): 1 for line in f if valid(line.lower().strip()) }
-for key,value in lines.iteritems():
-    print key
